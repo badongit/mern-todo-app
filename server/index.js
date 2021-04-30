1e9b3f19e9f4b0b689787572c96a440118200e09
unknown <nguyenbadong01@gmail.com>
2021-04-30 17:01:01 +0700

    Done

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = require('./config/database');
 
@@ -14,7 +16,7 @@ connectDB();
 
 router(app);
 
-const PORT = procress.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
